a2e85dd96f374d5615e67ba41923225f8f73f227
yjo78 <127858942+OwlEdu@users.noreply.github.com>
2026-02-04 02:09:40 +0900

    project description update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -23,7 +23,7 @@ export const projects: Project[] = [
     slug: 'connectrip',
     title: 'Connectrip',
     description: '여행지 소개, 여행자 매칭 커뮤니티 웹사이트 개발',
-    fullDescription: '여행자들을 위한 소셜 네트워킹 플랫폼입니다. 사용자들이 여행지를 공유하고, 함께 여행할 동반자를 찾을 수 있는 서비스를 개발했습니다. 여행지 소개, 여행지 리뷰, 여행자 커뮤니티 기능을 구현했습니다.',
+    fullDescription: '여행자들을 위한 소셜 네트워킹 플랫폼입니다. 사용자들이 여행지를 공유하고, 함께 여행할 동반자를 찾을 수 있는 서비스를 개발했습니다. 여행지 소개, 여행지 리뷰, 여행자 커뮤니티 기능을 구현했습니다. 이 프로젝트에서 저는 MySQL DB 설계 및 구축하는 역할과 Node.js로 백엔드 서버를 구축하고 REST API, JWT토큰을 통한 로그인 기능 및 로그인 상태, DB연결 등을 관리하는 역할을 맡았습니다.',
     tech: ['React', 'CSS', 'Node.js', 'MySQL'],
     images: [
       '/images/projects/connectrip-1.png',
@@ -54,7 +54,7 @@ export const projects: Project[] = [
     slug: 'smart-fems',
     title: '스마트 FEMS 프로젝트',
     description: 'React와 Node.js를 활용한 풀스택 일정 관리 애플리케이션',
-    fullDescription: '공장 에너지 관리 시스템(Factory Energy Management System)를 웹 앱의 형태로 구현했습니다. 실시간 에너지 사용량 모니터링, 데이터 시각화, AI모델을 활용한 추후 에너지 사용량 예측 등을 구현하여 에너지 효율을 최적화하는 데 기여했습니다. 본 프로젝트로 \'인공지능 기술을 활용한 에너지 사용량 계측 웹 어플리케이션 설계 및 구현\' 이라는 논문은 기재하여 2024년 한국정보기술학회 추계종합학술대회에서 동상을 수상하였습니다.',
+    fullDescription: '공장 에너지 관리 시스템(Factory Energy Management System)를 웹 앱의 형태로 구현했습니다. 실시간 에너지 사용량 모니터링, 데이터 시각화, AI모델을 활용한 추후 에너지 사용량 예측 등을 구현하여 에너지 효율을 최적화하는 것을 목적으로 만들어졌습니다. 해당 프로젝트에서 저는 웹 앱에 기능을 제공하는 용도의 Node.js 서버와 AI예측값을 제공하는 용도의 Django 서버를 구축/관리하여 프론트엔드에서의 공장별 로그인, 예측값 데이터시각화 등의 기능이 원활히 이루어지는 것에 기여하였습니다. 또한 본 프로젝트로 \'인공지능 기술을 활용한 에너지 사용량 계측 웹 어플리케이션 설계 및 구현\' 이라는 논문을 기재하여 2024년 한국정보기술학회 추계종합학술대회에서 동상을 수상하였습니다.',
     tech: ['React Typescript','Node.js', 'MySQL InfluxDB', 'Pytorch'],
     images: [
       '/images/projects/fems-1.png',
